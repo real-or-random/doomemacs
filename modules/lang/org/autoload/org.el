real-or-random/doomemacs

commit bb3431ad596c4908a8287d220b40e89e26c53f6b

fix(org): make sure org-reveal is called in the right buffer
Sometimes, `org-reveal` is called in the wrong buffer which throws an
error.  For example, `org-link-open-from-string` creates an temporary
org-mode buffer that gets killed very quickly which means that
`org-reveal` gets called in a different buffer.  I have also had issues
with org-reveal getting called in the org-roam buffer, which is why this
commit also saves the buffer it was called in.

modules/lang/org/autoload/org.el
@@ -508,7 +508,10 @@ All my (performant) foldings needs are met between this and `org-show-subtree'
        ;; Must be done on a timer because `org-show-set-visibility' (used by
        ;; `org-reveal') relies on overlays that aren't immediately available
        ;; when `org-mode' first initializes.
-       (run-at-time 0.1 nil #'org-reveal '(4))))
+       (let ((buf (current-buffer)))
+         (unless (doom-temp-buffer-p buf)
+           (run-at-time 0.1 nil (λ! (with-current-buffer buf
+                                      (org-reveal '(4)))))))))
 
 ;;;###autoload
 (defun +org-remove-occur-highlights-h ()

commit 89f7f745046dff791d478a4b24b172a03217c45a

fix(org): don't call org-reveal in dead buffer
This fixes a bug introduced in bb3431a (#7509). This shows up for
example in `org-capture`, which uses multiple org buffers and the
initial one (with name `*Capture*`) will be dead already by the time the
timer runs.

modules/lang/org/autoload/org.el
@@ -511,8 +511,9 @@ All my (performant) foldings needs are met between this and `org-show-subtree'
        (let ((buf (current-buffer)))
          (unless (doom-temp-buffer-p buf)
            (run-at-time 0.1 nil (lambda ()
-                                  (with-current-buffer buf
-                                    (org-reveal '(4)))))))))
+                                  (when (buffer-live-p buf)
+                                    (with-current-buffer buf
+                                      (org-reveal '(4))))))))))
 
 ;;;###autoload
 (defun +org-remove-occur-highlights-h ()